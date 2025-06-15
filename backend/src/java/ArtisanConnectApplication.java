ac255c8b3d91e1713103bf2ebf525164058a3096
Rosy <rosymartcabrera@gmail.com>
2025-06-15 13:55:02 -0400

    Created ArtisanConnectApplication.java

---
+++ backend/src/java/ArtisanConnectApplication.java
@@ -0,0 +1,13 @@
+package org.group5.artisanconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtisanConnectApplication.class, args);
+    }
+
+}
